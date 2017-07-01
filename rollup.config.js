787cd902f68cf857ca8afc422fabcc83d14566a2
Ffloriel <florielfedry@gmail.com>
2017-07-01 17:16:32 +0100

    config files and package.json

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from "rollup-plugin-babel"
+import builtins from "rollup-plugin-node-builtins"
+import commonjs from "rollup-plugin-commonjs"
+import resolve from "rollup-plugin-node-resolve"
+import flow from "rollup-plugin-flow"
+
+export default {
+    entry: "src/index.js",
+    targets: [
+        {
+            dest: "lib/purgecss.es.js",
+            format: "es"
+        },
+        {
+            dest: "lib/purgecss.js",
+            format: "cjs"
+        }
+    ],
+    plugins: [builtins(), resolve(), flow(), commonjs(), babel()],
+    external: ["postcss", "postcss-selector-parser"],
+    sourceMap: false
+}
