e133b123d12ad934ad72a94d6a6ea0ee1523b2bf
Floriel Fedry <Ffloriel@users.noreply.github.com>
2017-07-06 22:23:00 +0200

    initial index.js

---
+++ packages/gulp-purgecss/index.js
@@ -0,0 +1,43 @@
+import through from 'through2'
+import gulpUtil from 'gulp-util'
+import Purgecss from 'purgecss'
+
+const PLUGIN_NAME = 'gulp-purgecss'
+
+const gulpPurgecss = options => {
+    return through.obj((file, encoding, callback) => {
+        // empty
+        if (file.isNull()) return callback(null, file)
+        // buffer
+        if (file.isBuffer()) {
+            try {
+                const optionsGulp = Object.assign(options, {
+                    css: [file.contents.toString()]
+                })
+                const result = new Purgecss(optionsGulp).purge()[0].css
+                files.contents = new Buffer(result)
+                callback(null, file)
+            } catch (e) {
+                this.emit('error', new gulpUtil.PluginError(PLUGIN_NAME, e.message))
+            }
+        }
+        // stream
+        if (file.isStream()) {
+            var css = ''
+            file
+                .on('readable', buffer => {
+                    css += buffer.read().toString()
+                })
+                .on('end', () => {
+                    try {
+                        const optionsGulp = Object.assign(options, { css: [css] })
+                        const result = new Purgecss(optionsGulp).purge()[0].css
+                        file.contents = new Buffer(result)
+                        callback(null, file)
+                    } catch (e) {
+                        this.emit('error', new gulpUtil.PluginError(PLUGIN_NAME, e.message))
+                    }
+                })
+        }
+    })
+}
