9023fc2363be78e8b64506874a52e6300d0a0cbe
Floriel Fedry <Ffloriel@users.noreply.github.com>
2017-07-09 01:04:32 +0200

    babel rollup config

---
+++ packages/rollup-plugin-purgecss/rollup.config.js
@@ -0,0 +1,24 @@
+import babel from "rollup-plugin-babel"
+import commonjs from "rollup-plugin-commonjs"
+import resolve from "rollup-plugin-node-resolve"
+
+export default {
+    entry: "./src/index.js",
+    targets: [
+        {
+            dest: "./lib/rollup-plugin-purgecss.es.js",
+            format: "es"
+        },
+        {
+            dest: "./lib/rollup-plugin-purgecss.js",
+            format: "cjs"
+        }
+    ],
+    plugins: [
+        resolve(), commonjs(), babel()
+    ],
+    external: [
+        "purgecss",
+        "rollup-pluginutils"
+    ]
+}
