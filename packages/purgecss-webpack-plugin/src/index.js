5a6737e7a5167f0ffeb7e4775f8cf3b101d022f7
Ffloriel <florielfedry@gmail.com>
2017-11-13 21:43:54 +0000

    change deprecated property, add purgecss options
    - let purgecss options go throught purgecss-webpack
    - replace deprecated property, removing the warning

--- packages/purgecss-webpack-plugin/src/index.js
+++ packages/purgecss-webpack-plugin/src/index.js
@@ -5,55 +5,53 @@ import * as parse from './parse'
 import * as search from './search'
 
 export default class PurgecssPlugin {
-    constructor(options) {
-        this.options = options
-    }
+  constructor(options) {
+    this.options = options
+  }
 
-    apply(compiler) {
-        compiler.plugin('this-compilation', compilation => {
-            const entryPaths = parse.entryPaths(this.options.paths)
+  apply(compiler) {
+    compiler.plugin('this-compilation', compilation => {
+      const entryPaths = parse.entryPaths(this.options.paths)
 
-            parse.flatten(entryPaths).forEach(p => {
-                if (!fs.existsSync(p))
-                    throw new Error(`Path ${p} does not exist.`)
-            })
+      parse.flatten(entryPaths).forEach(p => {
+        if (!fs.existsSync(p)) throw new Error(`Path ${p} does not exist.`)
+      })
 
-            compilation.plugin('additional-assets', cb => {
-                // Go through chunks and purge as configured
-                compilation.chunks.forEach(
-                    ({ name: chunkName, files, modules }) => {
-                        const assetsToPurge = search
-                            .assets(compilation.assets, ['.css'])
-                            .filter(asset => files.indexOf(asset.name) >= 0)
+      compilation.plugin('additional-assets', cb => {
+        // Go through chunks and purge as configured
+        compilation.chunks.forEach(chunk => {
+          const { name: chunkName, files } = chunk
+          const modules = chunk.mapModules(mod => mod)
+          const assetsToPurge = search
+            .assets(compilation.assets, ['.css'])
+            .filter(asset => files.indexOf(asset.name) >= 0)
 
-                        assetsToPurge.forEach(({ name, asset }) => {
-                            const filesToSearch = parse
-                                .entries(entryPaths, chunkName)
-                                .concat(
-                                    search.files(
-                                        modules,
-                                        this.options.moduleExtensions || [],
-                                        file => file.resource
-                                    )
-                                )
-                                .filter(v => !v.endsWith('.css'))
-
-                            // Compile through Purgecss and attach to output.
-                            // This loses sourcemaps should there be any!
-                            const purgecss = new Purgecss({
-                                content: filesToSearch,
-                                css: [asset.source()],
-                                stdin: true
-                            })
-                            compilation.assets[name] = new ConcatSource(
-                                purgecss.purge()[0].css
-                            )
-                        })
-                    }
+          assetsToPurge.forEach(({ name, asset }) => {
+            const filesToSearch = parse
+              .entries(entryPaths, chunkName)
+              .concat(
+                search.files(
+                  modules,
+                  this.options.moduleExtensions || [],
+                  file => file.resource
                 )
+              )
+              .filter(v => !v.endsWith('.css'))
 
-                cb()
+            // Compile through Purgecss and attach to output.
+            // This loses sourcemaps should there be any!
+            const purgecss = new Purgecss({
+              ...this.options,
+              content: filesToSearch,
+              css: [asset.source()],
+              stdin: true
             })
+            compilation.assets[name] = new ConcatSource(purgecss.purge()[0].css)
+          })
         })
-    }
+
+        cb()
+      })
+    })
+  }
 }
