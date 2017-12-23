bf3cedd3fca7759006b92be9b9de7b80c62cb394
Ffloriel <florielfedry@gmail.com>
2017-12-23 18:34:05 +0000

    add test with only option
    - add integration test with only option based on the example

---
+++ packages/purgecss-webpack-plugin/__tests__/cases/simple-with-exclusion/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const glob = require('glob')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const PurgecssPlugin = require('../../../src/').default
+
+class CustomExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:/]+/g)
+  }
+}
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    legacy: './src/legacy.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?sourceMap'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css?[hash]'),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/*`),
+      styleExtensions: ['.css'],
+      whitelist: ['whitelisted'],
+      only: ['bundle'],
+      extractors: [
+        {
+          extractor: CustomExtractor,
+          extensions: ['html', 'js']
+        }
+      ]
+    })
+  ]
+}
