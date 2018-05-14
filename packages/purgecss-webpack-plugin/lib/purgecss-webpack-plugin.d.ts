1e37a22cbdafa633f3de5355154050911560269d
Brendan Mullins <megamaaaan@gmail.com>
2018-05-14 11:31:02 +0200

    Merge pull request #26 from JounQin/feat/types
    feat: add declaration file to work with webpack.config.ts

---
+++ packages/purgecss-webpack-plugin/lib/purgecss-webpack-plugin.d.ts
@@ -0,0 +1,29 @@
+// Type definitions for purgecss-webpack-plugin 0.22.0
+// Project: https://github.com/FullHuman/purgecss-webpack-plugin
+// Definitions by: JounQin https://github.com/JounQin
+
+import { Options as PurgecssOptions } from 'purgecss'
+import { Plugin } from 'webpack'
+
+// workaround to override Options of purgecess
+interface _Options extends Partial<PurgecssOptions> {
+  css?: never
+  content?: never
+  whitelist?: any
+  whitelistPatterns?: any
+}
+
+declare namespace PurgecssWebpackPlugin {
+  interface Options extends _Options {
+    paths: string[] | (() => string[])
+    only?: string[]
+    whitelist?: string[] | (() => string[])
+    whitelistPatterns?: RegExp[] | (() => RegExp[])
+  }
+}
+
+declare class PurgecssWebpackPlugin extends Plugin {
+  constructor(options: PurgecssWebpackPlugin.Options)
+}
+
+export = PurgecssWebpackPlugin
