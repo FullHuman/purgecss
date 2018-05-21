c5aa69b97529e767585944a6dc96094716709b41
Ffloriel <florielfedry@gmail.com>
2018-05-21 12:46:56 +0100

    update dependencies
    - update dependencies (postcss-selector-parser)
    - remove rollup-plugin-uglify

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import babel from "rollup-plugin-babel"
 import builtins from "rollup-plugin-node-builtins"
 import resolve from "rollup-plugin-node-resolve"
 import flow from "rollup-plugin-flow"
-import uglify from "rollup-plugin-uglify"
 
 export default {
     input: "src/index.js",
@@ -16,6 +15,6 @@ export default {
             format: "cjs"
         }
     ],
-    plugins: [builtins(), resolve(), flow(), babel(), uglify()],
+    plugins: [builtins(), resolve(), flow(), babel()],
     external: ["postcss", "postcss-selector-parser", "fs", "glob"]
 }
