e2b5ea58fe35a97b867955ff4e02aad3ab7fc6d6
Nikita Dyomin <mykyta.domin@massive.se>
2019-08-13 17:37:09 +0200

    added ability to exclude current rule with a ignore comment placed inside of it

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,4 @@
+export const IGNORE_ANNOTATION_CURRENT = 'purgecss ignore current'
 export const IGNORE_ANNOTATION_NEXT = 'purgecss ignore'
 export const IGNORE_ANNOTATION_START = 'purgecss start ignore'
 export const IGNORE_ANNOTATION_END = 'purgecss end ignore'
