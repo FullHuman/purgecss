6f5d95829ca19bd8ab980ca415575166b23629d7
Ffloriel <florielfedry@gmail.com>
2020-06-13 14:54:58 +0100

    docs: add how to use with ant-design
    https://github.com/FullHuman/purgecss/issues/172

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,141 +1,142 @@
 module.exports = {
   head: [
     [
-      'link',
+      "link",
       {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      }
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png'
-      }
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png'
-      }
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'manifest',
-        href: '/site.webmanifest'
-      }
+        rel: "manifest",
+        href: "/site.webmanifest",
+      },
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'mask-icon',
-        href: '/safari-pinned-tab.svg',
-        color: '#000000'
-      }
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#000000",
+      },
     ],
     [
-      'meta',
+      "meta",
       {
-        name: 'msapplication-TileColor',
-        content: '#ffffff'
-      }
+        name: "msapplication-TileColor",
+        content: "#ffffff",
+      },
     ],
     [
-      'meta',
+      "meta",
       {
-        name: 'theme-color',
-        content: '#ffffff'
-      }
+        name: "theme-color",
+        content: "#ffffff",
+      },
     ],
     [
-      'script',
+      "script",
       {
         async: true,
-        src: 'https://www.googletagmanager.com/gtag/js?id=UA-117085901-1'
-      }
+        src: "https://www.googletagmanager.com/gtag/js?id=UA-117085901-1",
+      },
     ],
     [
-      'script',
+      "script",
       {},
       `window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
   
-    gtag('config', 'UA-117085901-1');`
-    ]
+    gtag('config', 'UA-117085901-1');`,
+    ],
   ],
   themeConfig: {
-    logo: 'https://i.imgur.com/UEiUiJ0.png',
-    searchPlaceholder: 'Search...',
-    repo: 'FullHuman/purgecss',
-    docsDir: 'docs',
-    docsBranch: 'master',
+    logo: "https://i.imgur.com/UEiUiJ0.png",
+    searchPlaceholder: "Search...",
+    repo: "FullHuman/purgecss",
+    docsDir: "docs",
+    docsBranch: "master",
     editLinks: true,
-    editLinkText: 'Help us improve this page!',
+    editLinkText: "Help us improve this page!",
     evergreen: true,
     plugins: [
       [
-        '@vuepress/google-analytics',
+        "@vuepress/google-analytics",
         {
-          ga: 'UA-117085901-1'
-        }
-      ]
+          ga: "UA-117085901-1",
+        },
+      ],
     ],
     sidebar: {
-      '/': [
+      "/": [
         {
-          title: 'PurgeCSS',
+          title: "PurgeCSS",
           collapsable: false,
           children: [
-            ['', 'About PurgeCSS'],
-            ['configuration', 'Configuration'],
-            ['CLI', 'Command Line Interface'],
-            ['api', 'Programmatic API'],
-            ['whitelisting', 'Whitelisting'],
-            ['extractors', 'Extractors']
-          ]
+            ["", "About PurgeCSS"],
+            ["configuration", "Configuration"],
+            ["CLI", "Command Line Interface"],
+            ["api", "Programmatic API"],
+            ["whitelisting", "Whitelisting"],
+            ["extractors", "Extractors"],
+          ],
         },
         {
-          title: 'Plugins',
+          title: "Plugins",
           collapsable: false,
           children: [
-            ['plugins/postcss', 'PostCSS'],
-            ['plugins/webpack', 'Webpack'],
-            ['plugins/gulp', 'Gulp'],
-            ['plugins/grunt', 'Grunt'],
-            ['plugins/gatsby', 'Gatsby']
-          ]
+            ["plugins/postcss", "PostCSS"],
+            ["plugins/webpack", "Webpack"],
+            ["plugins/gulp", "Gulp"],
+            ["plugins/grunt", "Grunt"],
+            ["plugins/gatsby", "Gatsby"],
+          ],
         },
         {
-          title: 'Guides',
+          title: "Guides",
           collapsable: false,
           children: [
-            ['guides/vue', 'Vue'],
-            ['guides/react', 'React'],
-            ['guides/next', 'Next.js'],
-            ['guides/nuxt', 'Nuxt.js'],
-            ['guides/razzle', 'Razzle'],
-            ['guides/wordpress', 'WordPress']
-          ]
+            ["guides/vue", "Vue"],
+            ["guides/react", "React"],
+            ["guides/next", "Next.js"],
+            ["guides/nuxt", "Nuxt.js"],
+            ["guides/razzle", "Razzle"],
+            ["guides/wordpress", "WordPress"],
+          ],
         },
-        ['comparison', 'Comparison'],
+        ["comparison", "Comparison"],
         {
-          title: 'Common Questions',
+          title: "Common Questions",
           collapsable: false,
           children: [
             // ["node_modules", "How to use with node modules"],
-            ['css_modules', 'How to use with CSS modules']
-          ]
-        }
-      ]
-    }
-  }
-}
+            ["css_modules", "How to use with CSS modules"],
+            ["ant_design", "How to use with Ant Design"],
+          ],
+        },
+      ],
+    },
+  },
+};
