646e419cac01b3e647cc77ffebcfd5c31d087e23
Vincent Taing <taing.vincent@gmail.com>
2021-07-31 22:15:14 +0200

    fix(grunt-purgecss): Fix plugin not ouputting all files (#723)
    * Add additional .css files
    
    * Add failling test: grunt not processing all files
    
    * Wait for all files being purged before returning the result
    
    * Manual code styling

--- packages/grunt-purgecss/Gruntfile.js
+++ packages/grunt-purgecss/Gruntfile.js
@@ -9,6 +9,9 @@ module.exports = grunt => {
           content: ['./__tests__/fixtures/src/simple/**/*.html']
         },
         files: {
+          '__tests__/tmp/menu.css': ['__tests__/fixtures/src/menu.css'],
+          '__tests__/tmp/profile.css': ['__tests__/fixtures/src/profile.css'],
+          '__tests__/tmp/footer.css': ['__tests__/fixtures/src/footer.css'],
           '__tests__/tmp/simple.css': ['__tests__/fixtures/src/simple/simple.css']
         }
       }
