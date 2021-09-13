efe0eda7ced10c9a80f56bb44c097ff7c64f75a3
Kevin Ramharak <kevin@ramharak.nl>
2021-09-13 13:13:53 +0200

    setup basic test

---
+++ packages/purgecss/__tests__/rejectedCss.test.ts
@@ -0,0 +1,19 @@
+import PurgeCSS from "./../src/index";
+import { ROOT_TEST_EXAMPLES } from "./utils";
+
+describe("rejectedCss", () => {
+  it("returns the rejected css as part of the result", async () => {
+    expect.assertions(1);
+    const resultsPurge = await new PurgeCSS().purge({
+      content: [`${ROOT_TEST_EXAMPLES}rejectedCss/simple.js`],
+      css: [`${ROOT_TEST_EXAMPLES}rejectedCss/simple.css`],
+      rejectedCss: true,
+    });
+    const expected = `
+.rejected {
+    color: blue;
+}
+`;
+    expect(resultsPurge[0].rejectedCss).toBe(expected);
+  });
+});
