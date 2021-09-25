be189b9be0f3704d5d7e5caba87601696b4ebcdc
Kevin Ramharak <kevin@ramharak.nl>
2021-09-25 10:00:45 +0200

    test if rejected and rejectedCss stay in sync

--- packages/purgecss/__tests__/rejectedCss.test.ts
+++ packages/purgecss/__tests__/rejectedCss.test.ts
@@ -15,4 +15,14 @@ describe("rejectedCss", () => {
 }`;
     expect(resultsPurge[0].rejectedCss?.trim()).toBe(expected.trim());
   });
+  it("contains the rejected selectors as part of the rejected css", async () => {
+    expect.assertions(1);
+    const resultsPurge = await new PurgeCSS().purge({
+      content: [`${ROOT_TEST_EXAMPLES}rejectedCss/simple.js`],
+      css: [`${ROOT_TEST_EXAMPLES}rejectedCss/simple.css`],
+      rejected: true,
+      rejectedCss: true,
+    });
+    expect(resultsPurge[0].rejectedCss?.trim()).toContain(resultsPurge[0].rejected?.[0]);
+  });
 });
