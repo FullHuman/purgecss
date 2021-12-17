175dc025d6ae796a02292e5f3cd2fc2953f568d7
Quidam74 <toutlemonde74@gmail.com>
2021-12-17 12:53:25 +0100

    emit/update name-rejected.css file near name.css

--- packages/purgecss-webpack-plugin/src/index.ts
+++ packages/purgecss-webpack-plugin/src/index.ts
@@ -1,9 +1,9 @@
 import * as fs from "fs";
 import path from "path";
-import PurgeCSS, { defaultOptions } from "purgecss";
-import { Compilation, Compiler } from "webpack";
-import { ConcatSource } from "webpack-sources";
-import { PurgedStats, UserDefinedOptions } from "./types";
+import PurgeCSS, {defaultOptions} from "purgecss";
+import {Compilation, Compiler} from "webpack";
+import {ConcatSource} from "webpack-sources";
+import {PurgedStats, UserDefinedOptions} from "./types";
 
 const styleExtensions = [".css", ".scss", ".styl", ".sass", ".less"];
 const pluginName = "PurgeCSS";
@@ -13,10 +13,10 @@ const pluginName = "PurgeCSS";
  * @param fileName file name
  */
 function getFormattedFilename(fileName: string): string {
-  if (fileName.includes("?")) {
-    return fileName.split("?").slice(0, -1).join("");
-  }
-  return fileName;
+    if (fileName.includes("?")) {
+        return fileName.split("?").slice(0, -1).join("");
+    }
+    return fileName;
 }
 
 /**
@@ -25,111 +25,121 @@ function getFormattedFilename(fileName: string): string {
  * @param extensions extensions
  */
 function isFileOfTypes(filename: string, extensions: string[]): boolean {
-  const extension = path.extname(getFormattedFilename(filename));
-  return extensions.includes(extension);
+    const extension = path.extname(getFormattedFilename(filename));
+    return extensions.includes(extension);
 }
 
 export default class PurgeCSSPlugin {
-  options: UserDefinedOptions;
-  purgedStats: PurgedStats = {};
+    options: UserDefinedOptions;
+    purgedStats: PurgedStats = {};
 
-  constructor(options: UserDefinedOptions) {
-    this.options = options;
-  }
+    constructor(options: UserDefinedOptions) {
+        this.options = options;
+    }
 
-  apply(compiler: Compiler): void {
-    compiler.hooks.compilation.tap(
-      pluginName,
-      this.initializePlugin.bind(this)
-    );
-  }
+    apply(compiler: Compiler): void {
+        compiler.hooks.compilation.tap(
+            pluginName,
+            this.initializePlugin.bind(this)
+        );
+    }
 
-  initializePlugin(compilation: Compilation): void {
-    compilation.hooks.additionalAssets.tapPromise(pluginName, () => {
-      const entryPaths =
-        typeof this.options.paths === "function"
-          ? this.options.paths()
-          : this.options.paths;
+    initializePlugin(compilation: Compilation): void {
+        compilation.hooks.additionalAssets.tapPromise(pluginName, () => {
+            const entryPaths =
+                typeof this.options.paths === "function"
+                    ? this.options.paths()
+                    : this.options.paths;
 
-      entryPaths.forEach((p) => {
-        if (!fs.existsSync(p)) throw new Error(`Path ${p} does not exist.`);
-      });
+            entryPaths.forEach((p) => {
+                if (!fs.existsSync(p)) throw new Error(`Path ${p} does not exist.`);
+            });
 
-      return this.runPluginHook(compilation, entryPaths);
-    });
-  }
+            return this.runPluginHook(compilation, entryPaths);
+        });
+    }
 
-  async runPluginHook(
-    compilation: Compilation,
-    entryPaths: string[]
-  ): Promise<void> {
-    const assetsFromCompilation = Object.entries(compilation.assets).filter(
-      ([name]) => {
-        return isFileOfTypes(name, [".css"]);
-      }
-    );
-
-    for (const chunk of compilation.chunks) {
-      const assetsToPurge = assetsFromCompilation.filter(([name]) => {
-        if (this.options.only) {
-          return this.options.only.some((only) => name.includes(only));
-        }
-
-        return Array.isArray(chunk.files)
-          ? chunk.files.includes(name)
-          : chunk.files.has(name);
-      });
-
-      for (const [name, asset] of assetsToPurge) {
-        const filesToSearch = entryPaths.filter(
-          (v) => !styleExtensions.some((ext) => v.endsWith(ext))
+    async runPluginHook(
+        compilation: Compilation,
+        entryPaths: string[]
+    ): Promise<void> {
+        const assetsFromCompilation = Object.entries(compilation.assets).filter(
+            ([name]) => {
+                return isFileOfTypes(name, [".css"]);
+            }
         );
 
-        // Compile through Purgecss and attach to output.
-        // This loses sourcemaps should there be any!
-        const options = {
-          ...defaultOptions,
-          ...this.options,
-          content: filesToSearch,
-          css: [
-            {
-              raw: asset.source().toString(),
-            },
-          ],
-        };
+        for (const chunk of compilation.chunks) {
+            const assetsToPurge = assetsFromCompilation.filter(([name]) => {
+                if (this.options.only) {
+                    return this.options.only.some((only) => name.includes(only));
+                }
 
-        if (typeof options.safelist === "function") {
-          options.safelist = options.safelist();
+                return Array.isArray(chunk.files)
+                    ? chunk.files.includes(name)
+                    : chunk.files.has(name);
+            });
+
+            for (const [name, asset] of assetsToPurge) {
+                const filesToSearch = entryPaths.filter(
+                    (v) => !styleExtensions.some((ext) => v.endsWith(ext))
+                );
+
+                // Compile through Purgecss and attach to output.
+                // This loses sourcemaps should there be any!
+                const options = {
+                    ...defaultOptions,
+                    ...this.options,
+                    content: filesToSearch,
+                    css: [
+                        {
+                            raw: asset.source().toString(),
+                        },
+                    ],
+                };
+
+                if (typeof options.safelist === "function") {
+                    options.safelist = options.safelist();
+                }
+
+                if (typeof options.blocklist === "function") {
+                    options.blocklist = options.blocklist();
+                }
+
+                const purgecss = await new PurgeCSS().purge({
+                    content: options.content,
+                    css: options.css,
+                    defaultExtractor: options.defaultExtractor,
+                    extractors: options.extractors,
+                    fontFace: options.fontFace,
+                    keyframes: options.keyframes,
+                    output: options.output,
+                    rejected: options.rejected,
+                    rejectedCss: options.rejectedCss,
+                    variables: options.variables,
+                    safelist: options.safelist,
+                    blocklist: options.blocklist,
+                });
+                const purged = purgecss[0];
+                if (purged.rejected) {
+                    this.purgedStats[name] = purged.rejected;
+                }
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                let source: Source = new ConcatSource(purged.css)
+                compilation.updateAsset(name, source);
+                if (purged.rejectedCss !== undefined) {
+                    source = new ConcatSource(purged.rejectedCss)
+                    let rejectedName : string = name.replace('.css','-rejected.css')
+                    if(compilation.getAsset(rejectedName) === undefined){
+                        compilation.emitAsset(rejectedName, source);
+                    }else{
+                        compilation.updateAsset(rejectedName, source);
+                    }
+
+
+                }
+            }
         }
-
-        if (typeof options.blocklist === "function") {
-          options.blocklist = options.blocklist();
-        }
-
-        const purgecss = await new PurgeCSS().purge({
-          content: options.content,
-          css: options.css,
-          defaultExtractor: options.defaultExtractor,
-          extractors: options.extractors,
-          fontFace: options.fontFace,
-          keyframes: options.keyframes,
-          output: options.output,
-          rejected: options.rejected,
-          rejectedCss : options.rejectedCss,
-          variables: options.variables,
-          safelist: options.safelist,
-          blocklist: options.blocklist,
-        });
-        const purged = purgecss[0];
-
-        if (purged.rejected) {
-          this.purgedStats[name] = purged.rejected;
-        }
-
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        compilation.updateAsset(name, new ConcatSource(purged.css));
-      }
     }
-  }
 }
