f5285df88bdbeb4e4909d34243948c81852665f2
Floriel <Ffloriel@users.noreply.github.com>
2022-02-23 20:06:36 +0000

    test: create .temp folder if not exist

--- packages/purgecss/__tests__/cli/cli-multiple-files-output.test.ts
+++ packages/purgecss/__tests__/cli/cli-multiple-files-output.test.ts
@@ -1,10 +1,20 @@
 import { Command } from "commander";
 import { promises as asyncFs } from "fs";
+import * as path from 'path';
 import { parseCommandOptions, run } from "../../src/bin";
 import { CLI_TEST_FOLDER } from "../utils";
 
 describe("PurgeCSS CLI file output", () => {
   const program = parseCommandOptions(new Command());
+
+  beforeAll(async () => {
+    const tempFolder = path.resolve(CLI_TEST_FOLDER, '.temp');
+    try {
+      await asyncFs.access(tempFolder);
+    } catch {
+      await asyncFs.mkdir(tempFolder, { recursive: true })
+    }
+  })
 
   it("should output the result into a file if there's one result", async () => {
     program.parse([
